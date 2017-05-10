aa20f4b17bef77f1acd20d575c13ea006f91f63a
tychota <tychota@users.noreply.github.com>
2017-05-10 19:18:40 +0200

    fix(flow): fix missing declaration of flowtype

--- koa/flow/context.js
+++ koa/flow/context.js
@@ -1,19 +1,9 @@
 const DataLoader = require('dataloader').default;
 
 declare type ContextType = {
-  models: {
-    book: {
-      getById: (id: string) => Promise<BookType>,
-      getAll: () => Promise<Array<BookType>>,
-      getByBorrowerId: (id: string) => Promise<BookType>
-    },
-    bammer: {
-      getById: (id: string) => Promise<BammerType>,
-      getAll: () => Promise<Array<BammerType>>
-    }
-  },
   dataloaders: {
-    book: DataLoader,
-    bammer: DataLoader
-  }
+    book: { byId: DataLoader, primeLoaders: Function },
+    bammer: { byId: DataLoader, primeLoaders: Function }
+  },
+  state: { user?: { email: string } }
 };
